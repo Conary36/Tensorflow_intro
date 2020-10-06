fdc707f7795e4bb11f78f8020c040a9d982e7d4e
Conary36 <cbeckford144@gmail.com>
2020-10-06 10:26:09 -0400

    initialized

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+import tensorflow as tf
+import numpy as np
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
